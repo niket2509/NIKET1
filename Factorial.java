90c71a4ed5d69adc68bb60b49800288b02daecc3
niket2509 <88085040+niket2509@users.noreply.github.com>
2022-09-13 12:44:23 +0530

    Update Factorial.java
    recent commit

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial {
     public static void main(String[] args) {
 
-        int num = 1;
+        int num = 10;
         long factorial = 1;
         for(int i = 1; i <= num; ++i)
         {
